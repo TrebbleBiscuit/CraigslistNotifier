7fbd2e699156d1b83e1df6bdc1b4bdc6fc9bb895
TrebbleBiscuit <erik@dankmemes.local>
2017-08-03 18:20:29 -0400

    Build framework for multiple terms

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-#term = raw_input("Enter Search Term: ")
-term = "nintendo"
-
 import feedparser
-craigslist_nintendo_url = "https://cincinnati.craigslist.org/search/sss?format=rss&query=" + term
 
-rssfeed = feedparser.parse( craigslist_nintendo_url )
+terms = ["nintendo"]
 
-for x in rssfeed["entries"]:
-#    print x
-    for y in x:
-        if y == "summary" or y == "link":
-#            print "TITLE: " + y
-            print x[y]
-        elif y == "title":
-            posttitle = x[y]
-            print posttitle.replace('&#x0024;', '- Price: ')
-        elif y == "updated":
-            posttitle = x[y]
-            posttitle = posttitle.replace('T', ' - ')
-            posttitle = posttitle.replace('-05:00', '')
-            print posttitle
-    print ""
+for term in terms:
+    craigslist_search_url = "https://cincinnati.craigslist.org/search/sss?format=rss&query=" + term
+    rssfeed = feedparser.parse( craigslist_search_url )
+    for x in rssfeed["entries"]:
+    #    print x
+        for y in x:
+            if y == "summary" or y == "link":
+    #            print "TITLE: " + y
+                print x[y]
+            elif y == "title":
+                posttitle = x[y]
+                print posttitle.replace('&#x0024;', '- Price: ')
+            elif y == "updated":
+                posttitle = x[y]
+                posttitle = posttitle.replace('T', ' - ')
+                posttitle = posttitle.replace('-05:00', '')
+                print posttitle
+        print ""
+
+
+# This program just shoves all the listings on top of each other, I need to make them sort by time
